a11a5c57dedafef1f4ed33cf26cc2d1453c7d047
johnashu <johnashu1@hotmail.com>
2017-09-08 16:39:52 +0200

    ..

---
+++ search/hash_table.py
@@ -0,0 +1,22 @@
+"""
+Whenever an element is to be searched, compute the hash code of the key passed and locate the element using that hash code as index in the array. Use linear probing to get the element ahead if the element is not found at the computed hash code.
+
+"""
+
+
+a = h1 = {'0': '0', '1': '1', '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7',
+      '8': '8', '9': '9', '10': 'A', '11': 'B', '12': 'C', '13': 'D', '14': 'E', '15': 'F'}
+
+x = 'A'
+
+def maf_hash(array, value):
+    """
+
+
+    """
+
+          
+print("Results from For Loop:", maf_hash(a, x))
+
+
+
