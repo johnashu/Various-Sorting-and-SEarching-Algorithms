f15672e354d735adfcf6eca724a49bfdd6437413
johnashu <johnashu1@hotmail.com>
2017-10-18 13:11:18 +0200

    a

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -1,20 +1,21 @@
 """
-bubble sort algorithm
+Bubble Sort Algorithm
 
-This sorting algorithm is comparison-based algorithm in which each pair of adjacent elements is compared and the elements are swapped if they are not in order. This algorithm is not suitable for large data sets as its average and worst case complexity are of Ο(n2) where n is the number of items.
+This sorting algorithm is comparison-based algorithm in which each pair of adjacent elements is compared and the elements are swapped if they are not in order.<br><br> This algorithm is not suitable for large data sets as its average and worst case complexity are of Ο(n2) where n is the number of items.
 
 """
 
-unsort_list = [456, 456, 4, 89, 4956, 53, 12, 123, 345, 134, 23, 46, 37]
+unsort_list = [3, 12, 5, 4, 8, 5, 1]
 
 def maf_bubble(lst):
     """ Sorts a list using a bubblesort algorithm """
-    for p in range(len(lst)-1,0,-1):
+    for p in range(len(lst)-1, 0, -1):
         for i in range(p):
-            if lst[i] > lst[i+1]:
+            if lst[i] > lst[i + 1]:
                 t = lst[i]
-                lst[i] = lst[i+1]
-                lst[i+1] = t
+                lst[i] = lst[i + 1]
+                lst[i + 1] = t
+    return lst
 
-maf_bubble(unsort_list)
-print(unsort_list)
+#print(maf_bubble(unsort_list))
+
